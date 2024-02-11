03fb58385a75eddd88d92124f7c20d97f5398f8f
Anuj <anujtiwarimct_cse19@its.edu.in>
2024-02-11 23:50:40 +0530

    initial changes done..

---
+++ App2/App2/Page1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn(object sender, EventArgs e)
+        {
+            DisplayAlert("LOGIN", "Login Successful", "OK");
+            Navigation.PushAsync(new NavPage(usernameentry.Text));
+        }
+    }
+}
